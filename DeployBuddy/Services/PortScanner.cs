7845cb361da3f20ff8fdb3966b69c2f3e717d0ef
funkybit <funkyabhishek1@gmail.com>
2025-09-23 17:03:06 +0530

    Modified Port Scanner helpers

--- DeployBuddy/Services/PortScanner.cs
+++ DeployBuddy/Services/PortScanner.cs
@@ -1,24 +1,18 @@
-﻿using Microsoft.Web.Administration;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
 
 namespace DeployBuddy.Services
 {
+
+
     public class PortScanner
     {
         public List<int> GetUsedPorts()
         {
-            using var manager = new ServerManager();
-            return manager.Sites
-                .SelectMany(site => site.Bindings)
-                .Select(binding => binding.EndPoint?.Port ?? ParsePort(binding.BindingInformation))
-                .Distinct()
-                .ToList();
-        }
-
-        private int ParsePort(string bindingInfo)
-        {
-            // Format: IP:Port:HostHeader → e.g., "*:5001:"
-            var parts = bindingInfo.Split(':');
-            return parts.Length > 1 && int.TryParse(parts[1], out int port) ? port : -1;
+            var ipProps = IPGlobalProperties.GetIPGlobalProperties();
+            var listeners = ipProps.GetActiveTcpListeners();
+            return listeners.Select(ep => ep.Port).Distinct().ToList();
         }
 
         public List<int> GetConflictingPorts(IEnumerable<int> requiredPorts)
@@ -27,4 +21,5 @@ namespace DeployBuddy.Services
             return requiredPorts.Where(p => usedPorts.Contains(p)).ToList();
         }
     }
+
 }
